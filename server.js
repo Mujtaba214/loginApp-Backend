8758b62ff5c6e6577665ad7797f58c493b1784ec
Muhammad Mujtaba <mujtabausman21@gmail.com>
2025-05-11 21:11:25 +0500

    final commit

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ app.use(cors());
 app.use("/api/auth", authRoutes);
 
 connectDB();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

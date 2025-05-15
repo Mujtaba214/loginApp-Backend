3629834ab6df4f428961ab9984f731f73ecf4755
Muhammad Mujtaba <mujtabausman21@gmail.com>
2025-05-15 20:00:33 +0500

    Updated more texts now

--- controllers/authController.js
+++ controllers/authController.js
@@ -28,7 +28,7 @@ export const login = async (req, res) => {
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch)
-      return res.status(400).json({ message: "Invalid credentials" });
+      return res.status(400).json({ message: "Invalid credentialls" });
 
     res.json({
       user: { id: user._id, name: user.name, email: user.email },

d954c1903b77a4e9e59d5f4922ac1d2048121dda
Muhammad Mujtaba <mujtabausman21@gmail.com>
2025-06-03 15:31:00 +0500

    Updated text  now

--- controllers/authController.js
+++ controllers/authController.js
@@ -24,7 +24,7 @@ export const login = async (req, res) => {
 
   try {
     const user = await User.findOne({ email });
-    if (!user) return res.status(400).json({ message: "Invalid credentials applied here now" });
+    if (!user) return res.status(400).json({ message: "Invalid credentials applied" });
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch)

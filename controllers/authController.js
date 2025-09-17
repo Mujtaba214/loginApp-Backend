1b1236d7de28ed0284afcf3e8be8ab3af5be548f
Muhammad Mujtaba <mujtabausman21@gmail.com>
2025-09-17 15:09:11 +0500

    text

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,26 @@
+// controllers/authController.js
+import User from "../models/user.js";
+import bcrypt from "bcryptjs";
+
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if (user)
+      return res
+        .status(400)
+        .json({ message: "User already exists here apply loginn" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    user = new User({ name, email, password: hashedPassword });
+    await user.save();
+
+    res.status(201).json({ message: "User registered successfully here now" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 export const login = async (req, res) => {
   const { email, password } = req.body;

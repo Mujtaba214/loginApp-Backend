e6a2ad10567388d5c91639d32f7f1521e161f5b3
Muhammad Mujtaba <m.mujtabausman2005@gmail.com>
2026-02-13 18:19:50 +0500

    updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,48 @@
+// controllers/authController.js
+import User from "../models/user.js";
+import bcrypt from "bcryptjs";
 
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if (user)
+      return res
+        .status(400)
+        .json({ message: "User already exists here apply loginn" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    user = new User({ name, email, password: hashedPassword });
+    await user.save();
+
+    res.status(201).json({ message: "User registered successfully here now" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user)
+      return res
+        .status(400)
+        .json({ message: "Invalid credentials applied here now" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res
+        .status(401)
+        .json({ message: "Invalid credentialls applied here" });
+
+    res.json({
+      user: { id: user._id, name: user.name, email: user.email },
+      message: "Login successful",
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
